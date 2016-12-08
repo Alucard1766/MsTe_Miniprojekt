23f0b5f2ae6c3ea1dd0bf18f80322cb164f70d03
Luca Salzani <Alucard1766@users.noreply.github.com>
2016-12-08 14:50:41 +0100

    WiP

--- AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
+++ AutoReservation.BusinessLayer/AutoReservationBusinessComponent.cs
@@ -8,8 +8,52 @@ using System.Linq;
 namespace AutoReservation.BusinessLayer
 {
     public class AutoReservationBusinessComponent
-    {
- 
+    { 
+        public List<Auto> Autos
+        {
+            get
+            {
+                using (var context = new AutoReservationContext())
+                {
+                    var dbReturn = from c in context.Autos select c;
+                    List<Auto> autoList = new List<Auto>();
+                    foreach (Auto a in dbReturn)
+                    {
+                        autoList.Add(a);
+                    }
+                    return autoList;
+                }
+            }
+        }
+
+        //Kunden
+
+        //Reservationen
+
+        public Auto GetAutoById(int id)
+        {
+            using (var context = new AutoReservationContext())
+            {
+                var dbReturn = from a in context.Autos
+                               where a.Id == id
+                               select a;
+                
+                if (dbReturn.Count() == 1)
+                {
+                    return dbReturn.First();
+                }
+                else
+                {
+                    throw new System.Exception("no or too much auto for id"); //TODO: Wrong exception here
+                }
+            }
+        }
+
+
+        Auto InsertAuto(Auto auto)
+        {
+            //TODO: FOlie 64 im EntityFramework
+        }
 
         private static LocalOptimisticConcurrencyException<T> CreateLocalOptimisticConcurrencyException<T>(AutoReservationContext context, T entity)
             where T : class
