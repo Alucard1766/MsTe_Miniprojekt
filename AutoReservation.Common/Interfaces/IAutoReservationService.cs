df1c72b55fd1153a3c7b110dd3234612fede84b9
gubelli <a>
2016-12-08 13:57:02 +0100

    Add Lucas Work :+1:

--- AutoReservation.Common/Interfaces/IAutoReservationService.cs
+++ AutoReservation.Common/Interfaces/IAutoReservationService.cs
@@ -1,8 +1,39 @@
 ﻿using AutoReservation.Common.DataTransferObjects;
+using System.Collections.Generic;
+using System.ServiceModel;
 
 namespace AutoReservation.Common.Interfaces
 {
+    [ServiceContract]
     public interface IAutoReservationService
     {
+        [OperationContract]
+        List<AutoDto> getAllAuto();
+
+        [OperationContract]
+        List<KundeDto> getAllKunde();
+
+        [OperationContract]
+        List<ReservationDto> getAllReservation();
+
+
+
+        [OperationContract]
+        AutoDto getAuto(int id);
+
+        [OperationContract]
+        KundeDto getKunde(int id);
+
+        [OperationContract]
+        ReservationDto getReservation(int id);
+
+
+        [OperationContract]
+        void insertAuto(AutoDto auto);
+        [OperationContract]
+        void insertKunde(KundeDto kunde);
+        [OperationContract]
+        void insertReservation(ReservationDto reservation);
+
     }
 }
